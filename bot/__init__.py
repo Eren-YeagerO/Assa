fd3e9eea5fa7ad776e9af7e024b61be768442800
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-03-16 19:08:27 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -43,9 +43,7 @@ async def hello(bot, message):
 
 @bot.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
-    r_j_check = [u.id for u in message.new_chat_members]
-    if temp.ME in r_j_check:
-        if not await db.get_chat(message.chat.id):
+    await db.get_chat(message.chat.id):
             total = await bot.get_chat_members_count(message.chat.id)
             r_j = message.from_user.mention if message.from_user else "Anonymous"
             await bot.send_message(
